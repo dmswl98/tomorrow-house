3154fd87e82990fdd26f0e7e2cdbce636617279b
dmswl98 <chej098t@gmail.com>
2021-09-04 23:06:29 +0900

    ♻️ Extract common carousel options

--- js/carousels.js
+++ js/carousels.js
@@ -1,28 +1,28 @@
-const productCarousel = tns({
-  container: '.product-carousel .slider-list',
+const commonOptions = {
   controls: false,
-  navContainer: '.product-carousel .thumbnail-list',
-  navAsThumbnails: true,
   arrowKeys: true,
+  navAsThumbnails: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+}
+
+const productCarousel = tns({
+  ...commonOptions,
+  container: '.product-carousel .slider-list',
+  navContainer: '.product-carousel .thumbnail-list',
   autoplay: true,
   autoplayHoverPause: true,
   autoplayButtonOutput: false,
-  mouseDrag: true,
-  preventScrollOnTouch: true,
 })
 
 const userGallery = tns({
+  ...commonOptions,
   container: '.user-gallery .slider-list',
   gutter: 4,
   edgePadding: 16,
-  controls: false,
   controlsContainer: '.user-gallery-controls',
   navContainer: '.user-gallery .thumbnail-list',
-  navAsThumbnails: true,
   loop: false,
-  arrowKeys: true,
-  mouseDrag: true,
-  preventScrollOnTouch: true,
   responsive: {
     768: {
       gutter: 6,
